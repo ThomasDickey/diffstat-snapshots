0278d5e0ea91581145e7f464d63b1d09090988eb
Thomas E. Dickey <dickey@invisible-island.net>
1996-03-24 23:53:57 -0500

    snapshot of project "diffstat", label t19960324

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-# $Id: run_test.sh,v 1.2 1996/03/17 00:32:41 tom Exp $
+# $Id: run_test.sh,v 1.3 1996/03/24 01:29:23 tom Exp $
 # Test-script for DIFFSTAT
-for i in *.pat
+if [ $# = 0 ]
+then
+	eval $0 *.pat
+	exit
+fi
+PATH=`cd ..;pwd`:$PATH; export PATH
+
+for i in $*
 do
 	for j in "" "-p1" "-p9"
 	do
@@ -9,7 +16,7 @@ do
 		if [ ".$j" != "." ] ; then
 			N=$N`echo ./$j|sed -e 's@./-@@'`
 		fi
-		../diffstat $j $i >$N.out
+		diffstat $j $i >$N.out 2>$N.err
 		if [ -f $N.ref ]
 		then
 			if ( cmp -s $N.out $N.ref )
@@ -24,5 +31,6 @@ do
 			echo '** save: '$N
 			mv $N.out $N.ref
 		fi
+		rm -f $N.err
 	done
 done
